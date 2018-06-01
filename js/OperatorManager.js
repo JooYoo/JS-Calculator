ae71e7369ba82b85f95fbd8baf55ad8c306194e8
JooYoo <jooyoo@outlook.com>
2018-06-01 10:36:23 +0200

    tested splite jss

---
+++ js/OperatorManager.js
@@ -0,0 +1,9 @@
+
+
+// exports.opSayHello =  function (){
+//     console.log("Hello I'm OperatorManager.js");
+// }
+
+// export function opSayHello(){
+//     console.log("Hello I'm OperatorManager.js");
+// }
--- js/calculate.js
+++ js/calculate.js
@@ -1,14 +1,34 @@
 
-var divResult= document.getElementById("divResult");
+
+
+var divResult = document.getElementById("divResult");
 
 var btnNum1 = document.getElementById("btnNum1");
 var btnNum2 = document.getElementById("btnNum2");
 var btnNum3 = document.getElementById("btnNum3");
 var btnNum4 = document.getElementById("btnNum4");
+var btnNum5 = document.getElementById("btnNum5");
 
-console.log(divResult.innerHTML);
 
-function btnNum1Press(){divResult.innerHTML = btnNum1.innerHTML;}
-function btnNum2Press(){divResult.innerHTML = btnNum2.innerHTML;}
-function btnNum3Press(){divResult.innerHTML = btnNum3.innerHTML;}
-function btnNum4Press(){divResult.innerHTML = btnNum4.innerHTML;}
+
+
+var numLeft = [];
+
+function btnNum1Press() {
+    var btnNum1= 1;
+    divResult.innerHTML = btnNum1;
+    numLeft.push(btnNum1);
+    console.log(numLeft);
+
+    opSayHello();
+
+}
+
+
+function btnNum2Press() {
+    divResult.innerHTML = 2;
+}
+
+function btnNum3Press() { divResult.innerHTML = btnNum3.innerHTML; }
+function btnNum4Press() { divResult.innerHTML = btnNum4.innerHTML; }
+function btnNum5Press() { divResult.innerHTML = btnNum5.innerHTML; }
