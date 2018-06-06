2d64c7c7b26d89d711b8d24d85feb09cb7c167fc
JooYoo <jooyoo@outlook.com>
2018-06-06 08:51:41 +0200

    the basic operation works

--- js/calculate.js
+++ js/calculate.js
@@ -9,8 +9,28 @@ function btnNum2Press() { toDisplay(2); }
 function btnNum3Press() { toDisplay(3); }
 function btnNum4Press() { toDisplay(4); }
 function btnNum5Press() { toDisplay(5); }
+function btnNum6Press() { toDisplay(6); }
+function btnNum7Press() { toDisplay(7); }
+function btnNum8Press() { toDisplay(8); }
+function btnNum9Press() { toDisplay(9); }
+function btnNum0Press() { toDisplay(0); }
 
 function btnPlus() { toDisplay('+'); }
+function btnMinus() { toDisplay('-'); }
+function btnTimes() { toDisplay('*'); }
+function btnDivide() { toDisplay('/'); }
+function btnEqualTo(){
+    // calculate 
+    var result = eval(printInput);
+    console.log("[btnEqualto()] result: " + result);
+    // disply =
+    toDisplay('=');
+    // display result
+    toDisplay(result);
+    // reset calculator
+    tempoNums = [];
+}
+
 function btnReset(){ 
     console.log('[btnRset()]');
     // clean tempoNumber() container 
@@ -18,8 +38,6 @@ function btnReset(){
     // clean DOM up
     divResult.innerHTML = ' ';
 }
-
-
 
 function toDisplay(targetNum) {// a method to display Numbers
     // push 1 into array during each click
@@ -33,17 +51,6 @@ function toDisplay(targetNum) {// a method to display Numbers
     console.log("[toDisplay()] printInput: " + printInput);
 }
 
-function btnEqualTo(){
-    // calculate 
-    var result = eval(printInput);
-    console.log("[btnEqualto()] result: " + result);
-    // disply =
-    toDisplay('=');
-    // display result
-    toDisplay(result);
-    // reset calculator
-    tempoNums = [];
-}
 
 
 
